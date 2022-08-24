3450f74b67a448cfc9f855b157ca5f585b2e1dc4
xiaomingfor <wuzhiming@xmiles.cn>
2022-08-24 13:53:41 +0800

    feature：支持自定义redis序列化方式，原先的jdk序列化方式不受影响。

--- src/main/java/com/pig4cloud/plugin/cache/support/CacheMessageListener.java
+++ src/main/java/com/pig4cloud/plugin/cache/support/CacheMessageListener.java
@@ -4,7 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
 
 /**
  * @author fuwei.deng
@@ -14,13 +14,17 @@ import org.springframework.data.redis.core.RedisTemplate;
 @RequiredArgsConstructor
 public class CacheMessageListener implements MessageListener {
 
-	private final RedisTemplate<Object, Object> redisTemplate;
+	private RedisSerializer<Object> javaSerializer = RedisSerializer.java();
 
 	private final RedisCaffeineCacheManager redisCaffeineCacheManager;
 
 	@Override
 	public void onMessage(Message message, byte[] pattern) {
-		CacheMessage cacheMessage = (CacheMessage) redisTemplate.getValueSerializer().deserialize(message.getBody());
+
+		/**
+		 * 发送端固定了jdk序列户方式，接收端同样固定了jdk序列化方式进行反序列化。
+		 */
+		CacheMessage cacheMessage = (CacheMessage) javaSerializer.deserialize(message.getBody());
 		log.debug("recevice a redis topic message, clear local cache, the cacheName is {}, the key is {}",
 				cacheMessage.getCacheName(), cacheMessage.getKey());
 		redisCaffeineCacheManager.clearLocal(cacheMessage.getCacheName(), cacheMessage.getKey());
