5df96fb5ab10d75257cc4e431b02834f9909db3a
lishangbu <shangshili@hotmail.com>
2022-12-31 21:04:59 +0800

    :recycle: Refactoring code.优化注解配置

--- src/main/java/com/pig4cloud/plugin/cache/MultilevelCacheAutoConfiguration.java
+++ src/main/java/com/pig4cloud/plugin/cache/MultilevelCacheAutoConfiguration.java
@@ -8,13 +8,11 @@ import com.pig4cloud.plugin.cache.support.ServerIdGenerator;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
@@ -28,9 +26,7 @@ import java.util.Objects;
  * @author fuwei.deng
  * @version 1.0.0
  */
-@AutoConfiguration
-@Configuration(proxyBeanMethods = false)
-@AutoConfigureAfter(RedisAutoConfiguration.class)
+@AutoConfiguration(after = RedisAutoConfiguration.class)
 @EnableConfigurationProperties(CacheConfigProperties.class)
 public class MultilevelCacheAutoConfiguration {
 
